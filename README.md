dc82b75301e52ae0b4ddb35239dcc3a48a001d0b
larrykirschner <larrykirschner@gmail.com>
2018-03-04 13:57:39 -0800

    prep 1.0

---
+++ README.md
@@ -0,0 +1,1 @@
+GizmoSettings is a util component thats it easier to stencil gizmos for complex UI setups
---
+++ src/gizmo-settings/GizmoSettings.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using BeatThat;
+using BeatThat.ColorExtensions;
+
+namespace BeatThat
+{
+	
+	/// <summary>
+	/// Component makes it easy to enable/disable gizmos and associated colors on a per-object basis
+	/// </summary>
+	public class GizmoSettings : MonoBehaviour
+	{
+
+		public enum DrawGizmosDirective { Disable = 0, WhenSelected = 1, Always = 2 }
+		public DrawGizmosDirective m_drawGizmos = DrawGizmosDirective.WhenSelected;
+		public Color m_gizmoLineColor = Color.cyan;
+		public Color m_gizmoFillColor = Color.cyan.WithAlpha(0.1f);
+
+		void OnValidate()
+		{
+			this.gameObject.hideFlags = HideFlags.None;
+		}
+
+		#if UNITY_EDITOR
+		void OnDrawGizmosSelected()
+		{
+			if (m_drawGizmos == DrawGizmosDirective.WhenSelected) {
+				DoDrawGizmos ();
+			}
+		}
+
+		void OnDrawGizmos()
+		{
+			if (m_drawGizmos == DrawGizmosDirective.Always) {
+				DoDrawGizmos ();
+			}
+		}
+
+		private void DoDrawGizmos()
+		{
+			var rt = this.transform as RectTransform;
+			if (rt == null) {
+				return;
+			}
+
+			Rect r = rt.rect;
+
+			rt.DrawGizmoScreenRect(m_gizmoLineColor);
+			rt.DrawGizmoFillScreenRect(m_gizmoFillColor);
+		}
+		#endif
+	}
+}
